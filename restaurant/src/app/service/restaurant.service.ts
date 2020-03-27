7efcc44e2cd02e5965f22bfdbe4eec249f5dd523
Howlyhack <ibouu@hotmail.fr>
2020-03-27 12:28:39 +0000

    Avec le strapi

---
+++ restaurant/src/app/service/restaurant.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Restaurant } from '../Models/restaurant';
+import {URL} from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestaurantService {
+
+  constructor(private http: HttpClient) { }
+
+  getRestaurants( ): Observable<Restaurant[]> {
+    return this.http.get<Restaurant []>(URL + '/restaurants').pipe();
+
+  }
+
+  getRestaurant( id: number): Observable<Restaurant> {
+    return this.http.get<Restaurant>(URL + '/restaurants/' + id).pipe();
+  }
+
+  postRestaurant( restaurant: Restaurant): Observable<Restaurant> {
+    return this.http.post<Restaurant>(URL + '/restaurants', restaurant).pipe();
+  }
+
+  deleteRestaurant(id: number): Observable<Restaurant> {
+    return this.http.delete<Restaurant>(URL + '/restaurants/' + id).pipe();
+  }
+  updateRestaurant( restaurant: Restaurant) {
+    return this.http.put<Restaurant>(URL + '/restaurants/' + restaurant.id, restaurant).pipe();
+  }
+}
